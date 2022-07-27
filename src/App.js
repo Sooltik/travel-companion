3ca413e1b636f0f55362dcd59361c60e7b19cfe5
Sooltik <niazov.sultan@gmail.com>
2022-07-26 22:44:02 -0400

    Feat: add current location.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,17 @@ import Map from './components/Map/Map'
 const App = () => {
     const [places, setPlaces] = useState([])
 
+    const [coordinates, setCoordinates] = useState({ })
+    const [bounds, setBounds] = useState(null)
+
     useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+            setCoordinates({lat: latitude, lng: longitude})
+        })
+    }, [])
+
+    useEffect(() => {
+        console.log(coordinates, bounds);
         getPlacesData()
             .then((data) => {
                 console.log(data);
@@ -18,6 +28,7 @@ const App = () => {
     }, [])
 
     return (
+
         <>
             <CssBaseline />
             <Header />
@@ -26,7 +37,11 @@ const App = () => {
                     <List />
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    <Map />
+                    <Map
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates} />
+
                 </Grid>
             </Grid>
         </>
